d61e324f6bc6877c6c1012f5e7faaa3d4a842b81
Paulus Bangun <paulus.bangun@seamoney.com>
2023-01-23 16:10:19 +0700

    routing exp using servemux

--- routing/app/main.go
+++ routing/app/main.go
@@ -1,5 +1,31 @@
 package main
 
-func main() {
+import (
+	"errors"
+	"fmt"
+	"io"
+	"net/http"
+	"os"
+)
 
+func main() {
+	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
+		// if r.URL.Path != "/" {
+		// 	http.NotFound(w, r)
+		// 	return
+		// }
+		io.WriteString(w, r.URL.Path)
+	})
+
+	http.HandleFunc("/health", func(w http.ResponseWriter, r *http.Request) {
+		io.WriteString(w, "OK")
+	})
+
+	err := http.ListenAndServe(":3333", nil)
+	if errors.Is(err, http.ErrServerClosed) {
+		fmt.Printf("server closed\n")
+	} else if err != nil {
+		fmt.Printf("error starting server: %s\n", err)
+		os.Exit(1)
+	}
 }
---
+++ routing/readme.txt
@@ -0,0 +1,1 @@
+https://www.digitalocean.com/community/tutorials/how-to-make-an-http-server-in-go
